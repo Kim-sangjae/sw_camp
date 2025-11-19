02555be555468acdd228729c3aaee7d63b49c0dc
Kim-sangjae <kimsangjae94114@gmail.com>
2025-11-19 16:00:55 +0900

    vue update

---
+++ vue/02_vue_components-basics/src/components/02_slot/SlotChild.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--
+<slot>자리에는 부모가 <slotChild>...</SlotChild> 사이에 넓은 컨텐츠가 그대로 담기나
+-->
+Child가 전달 받은 slot =><slot></slot>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/02_vue_components-basics/src/components/03_scoped-slot/ScopedSlotParent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import ScopedSlotChild from "@/components/03_scoped-slot/ScopedSlotChild.vue";
+</script>
+
+<template>
+<div class="container">
+  <h2>03. Scoped Slot 테스트</h2>
+  <ScopedSlotChild>
+<!--
+# : v-slot 의 축약형
+#default : 자식에서 slot name을 별도로 지정하지 않았을 경우의 기본값
+{user} : 구조 분해 할당 문법으로 slotProps.user 로 자식이 전달한 user에 연결한다
+-->
+    <template #default="{user}">
+      <strong>{{user.name}}</strong> = <em>{{user.age}}세</em>
+    </template>
+  </ScopedSlotChild>
+</div>
+</template>
+
+<style scoped>
+
+</style>
