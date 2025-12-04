41ac0d8c1c32538778af8ef5d3dfefd0c3aa5218
Kim-sangjae <kimsangjae94114@gmail.com>
2025-12-04 15:35:30 +0900

    first commit

---
+++ devops/03_message-broker/kafka-example/jimWhere/src/main/java/com/jimwhere/JimWhereApplication.java
@@ -0,0 +1,13 @@
+package com.jimwhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimWhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimWhereApplication.class, args);
+    }
+
+}
---
+++ devops/03_message-broker/kafka-example/jimWhere/src/test/java/com/jimwhere/JimWhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimwhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimWhereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
